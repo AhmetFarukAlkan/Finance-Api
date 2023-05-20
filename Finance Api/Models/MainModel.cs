da698e6b23f22db18a18a7e3b3f56709570f12d6
Emirhan Çınar <emirhancinar025@gmail.com>
2023-05-20 14:18:43 +0300

    Proje dosyası ekle.

---
+++ Finance Api/Models/MainModel.cs
@@ -0,0 +1,13 @@
+﻿using Finance_Api.Controllers;
+
+namespace Finance_Api.Models
+{
+    public class MainModel
+    {
+
+        public SelectedItems selectedItems { get; set; }
+        public ExchangeRate exchange { get; set; }
+
+
+    }
+}
---
+++ Finance Api/Models/SelectedItems.cs
@@ -0,0 +1,9 @@
+﻿namespace Finance_Api.Models
+{
+    public class SelectedItems
+    {
+
+        public string currentFrom { get; set; }
+        public string currentTo { get; set; }
+    }
+}
